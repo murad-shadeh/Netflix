093e6c0cc953dec0e449c0444a540d9a35385cf4
murad-dabbous <moradyousef04@gmail.com>
2025-09-02 19:11:50 +0000

    fix(Nav): Add Navbar to favourite page

--- src/components/FavList.jsx
+++ src/components/FavList.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import ModalUpdate from "./UpdateModal";
+import NavBar from "./NavBar";
 
 export default function FavList() {
   const [movies, setMovies] = useState([]);
@@ -37,43 +38,49 @@ export default function FavList() {
   };
   const poster_pathURL = "http://image.tmdb.org/t/p/w500/";
   return (
-    <div>
-      <div className="row">
-        {movies.map((movie) => (
-          <div className="col-md-4" key={movie.id}>
-            <Card style={{ marginBottom: "20px" }}>
-              <Card.Img
-                variant="top"
-                src={poster_pathURL + movie.poster_path}
-              />
-              <Card.Body>
-                <Card.Title>{movie.title}</Card.Title>
-                <Card.Text>
-                  <br />
-                  {movie.overview}
-                  <br />
-                  Comments: {movie.comments}
-                </Card.Text>
-                <Button variant="danger" onClick={() => handleDelete(movie.id)}>
-                  Delete
-                </Button>
-                <Button variant="primary" onClick={() => handleUpdate(movie)}>
-                  {" "}
-                  Update{" "}
-                </Button>
-              </Card.Body>
-            </Card>
-          </div>
-        ))}
+    <>
+      <NavBar />
+      <div>
+        <div className="row">
+          {movies.map((movie) => (
+            <div className="col-md-4" key={movie.id}>
+              <Card style={{ marginBottom: "20px" }}>
+                <Card.Img
+                  variant="top"
+                  src={poster_pathURL + movie.poster_path}
+                />
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+                  <Card.Text>
+                    <br />
+                    {movie.overview}
+                    <br />
+                    Comments: {movie.comments}
+                  </Card.Text>
+                  <Button
+                    variant="danger"
+                    onClick={() => handleDelete(movie.id)}
+                  >
+                    Delete
+                  </Button>
+                  <Button variant="primary" onClick={() => handleUpdate(movie)}>
+                    {" "}
+                    Update{" "}
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
+        {selectedMovie && (
+          <ModalUpdate
+            movie={selectedMovie}
+            showModal={showModal}
+            setShowModal={setShowModal}
+            setMovies={setMovies}
+          />
+        )}
       </div>
-      {selectedMovie && (
-        <ModalUpdate
-          movie={selectedMovie}
-          showModal={showModal}
-          setShowModal={setShowModal}
-          setMovies={setMovies}
-        />
-      )}
-    </div>
+    </>
   );
 }
