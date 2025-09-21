a78ee0f2688c7915175c44b9b1402ca63013a70d
murad-dabbous <moradyousef04@gmail.com>
2025-09-21 22:37:38 +0000

    fix(Nav): fix the NavBar Styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
+
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
+        {/* aligned left */}
         <Navbar.Brand
           href="/"
           className="title"
@@ -13,42 +14,22 @@ const NavBar = () => {
         >
           Netflix
         </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link
-              href="/"
-              className="nav-link"
-              style={{
-                fontSize: "21px",
-              }}
-            >
+          {/* aligned right  usign ms-auto*/}
+          <Nav className="ms-auto">
+            <Nav.Link href="/" style={{ fontSize: "21px" }}>
               Home
             </Nav.Link>
-            <Nav.Link
-              href="/fav"
-              className="nav-link"
-              style={{
-                fontSize: "21px",
-              }}
-            >
+            <Nav.Link href="/fav" style={{ fontSize: "21px" }}>
               Favourites
             </Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 };
+
 export default NavBar;
